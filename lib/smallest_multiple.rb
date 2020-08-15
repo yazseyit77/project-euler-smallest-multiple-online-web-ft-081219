62d2ba9f46aba1a647df193eac704705d5fd0095
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2020-08-15 16:27:59 +0000

    Automatically backed up by Learn

--- lib/smallest_multiple.rb
+++ lib/smallest_multiple.rb
@@ -2,8 +2,10 @@
 
 def smallest_multiple(limit)
   multiple = 1
-  (2...limit).each do |d|
-    multiple *= d / gcd(multiple, d)
+  (2...limit).each do |divisor|
+    multiple *= divisor / gcd(multiple, divisor)
   end
   multiple
 end
+
+def gcm(multiple, divisor)
