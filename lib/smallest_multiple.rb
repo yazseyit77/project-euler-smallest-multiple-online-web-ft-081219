cfc7931db10ee29300c2190753469aa7a6cb9b97
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2020-08-15 16:26:44 +0000

    Automatically backed up by Learn

--- lib/smallest_multiple.rb
+++ lib/smallest_multiple.rb
@@ -1,1 +1,10 @@
 # Implement your procedural solution here!
+
+def smallest_multiple(limit)
+  multiple = 1
+  (2...limit).each do |d|
+    multiple *= d / gcd(multiple, d)
+  end 
+  multiple
+end
+
