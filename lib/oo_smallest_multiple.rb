1ab5c05d4660b1726c6d7d56e14d82bdd6df73d2
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2020-08-15 16:34:33 +0000

    Done.

--- lib/oo_smallest_multiple.rb
+++ lib/oo_smallest_multiple.rb
@@ -1,1 +1,28 @@
 # Implement your object-oriented solution here!
+
+class SmallestMultiple
+  attr_reader :lcm
+
+  def initialize(limit)
+    @limit = limit
+    @lcm = lcm
+  end
+
+  def lcm
+    multiple = 1
+    (2..@limit).each do |divisor|
+      multiple *= divisor / gcd(multiple, divisor)
+    end
+    multiple
+  end
+
+  def gcd(multiple, divisor)
+    while divisor > 0
+      multiple %= divisor
+      return divisor if multiple == 0
+      divisor %= multiple
+    end
+    multiple
+  end
+
+end
--- lib/smallest_multiple.rb
+++ lib/smallest_multiple.rb
@@ -1,1 +1,18 @@
 # Implement your procedural solution here!
+
+def smallest_multiple(limit)
+  multiple = 1
+  (2..limit).each do |divisor|
+    multiple *= divisor / gcd(multiple, divisor)
+  end
+  multiple
+end
+
+def gcd(multiple, divisor)
+  while divisor > 0
+    multiple %= divisor
+    return divisor if multiple == 0
+    divisor %= multiple
+  end
+  multiple
+end
